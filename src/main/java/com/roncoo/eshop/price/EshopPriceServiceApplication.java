4f276f7f39826a81729a87bd72d8c503c9235456
unknown <307249941@qq.com>
2019-01-10 16:19:55 +0800

    fix buger

--- src/main/java/com/roncoo/eshop/price/EshopPriceServiceApplication.java
+++ src/main/java/com/roncoo/eshop/price/EshopPriceServiceApplication.java
@@ -23,7 +23,7 @@ public class EshopPriceServiceApplication {
 		config.setMaxIdle(5);
 		config.setMaxWaitMillis(1000 * 10); 
 		config.setTestOnBorrow(true);
-		return new JedisPool(config, "localhost", 6379);
+		return new JedisPool(config, "192.168.1.111", 1111);
 	}
 	
 }
